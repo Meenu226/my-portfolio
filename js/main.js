083f0241918aae2f53dfc13fb7bc9d3ea9a91101
Meenu226 <92501610+Meenu226@users.noreply.github.com>
2022-06-26 00:25:55 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,91 @@
+// select loader hear 
+const loader = document.querySelector('#loader');
+const main = document.querySelector('.main')
+const sidebarMenu = document.querySelector('.sidebar__menu')
+const hamburgerIcon = document.getElementById('hamburger');
+const sidebarDisplay = document.getElementById('sidebar');
+const closeBtn = document.getElementById('close-menu');
+const blurClass = document.querySelectorAll('.blur-class');
+//Hide Loader 
+function init() {
+    setTimeout(() => {
+        // loader.classList.remove('show');
+        // loader.classList.add('hide');
+        loader.style.opacity = 0;
+        loader.style.display = 'none';
+
+        main.style.display = 'block';
+        setTimeout(()=>(main.style.opacity = 1),50)
+    }, 2000)
+}
+init();
+hamburgerIcon.addEventListener('click', (event) => {
+    sidebarDisplay.style.display = "flex";
+    sidebarMenu.style.display = "flex"
+    blurClass.forEach((element) => {
+        element.style.filter = "blur(4px)"
+    })
+    sidebarDisplay.style.animation = "slide-left 0.75s cubic-bezier(0.250, 0.460, 0.450, 0.940) both";
+
+
+})
+closeBtn.addEventListener('click', (event) => {
+    sidebarDisplay.style.animation = "slide-right 0.75s cubic-bezier(0.250, 0.460, 0.450, 0.940) both";
+    blurClass.forEach((element) => {
+        setTimeout(() => {
+            element.style.filter = "blur(0px)"
+            sidebarDisplay.style.display = "none";
+            sidebarMenu.style.display = "none"
+        }, 750)
+    })
+})
+
+const wordToShow = ["I love to learn", "I belive in smart work rather than hardwork ", "I love to cook in my spare time"]
+const dynamicHeading = document.getElementById("dynamic-text");
+
+
+
+function deleteChars(deletingSpeed) {
+    console.log(dynamicHeading)
+    const htmlText = dynamicHeading.innerHTML
+}
+deleteChars()
+
+const words = ["I love to learn.  ", "I belive in smart work rather than hardwork.  ", "I love to cook in my spare time.  "]
+let i = 0;
+let counter;
+function typeNow() {
+    let word = words[i].split("");
+    var loopTyping = function () {
+        if (word.length > 0) {
+            document.getElementById('dynamic-text').innerHTML += word.shift();
+
+        } else {
+            deleteNow();
+            return false;
+        };
+        counter = setTimeout(loopTyping, 50);
+    };
+    loopTyping();
+};
+function deleteNow() {
+    let word = words[i].split("");
+    var loopDeleting = function () {
+        if (word.length > 0) {
+            word.pop();
+            document.getElementById('dynamic-text').innerHTML = word.join("");
+        } else {
+            if (words.length > (i + 1)) {
+                i++;
+            }
+            else {
+                i = 0;
+            };
+            typeNow();
+            return false;
+        };
+        counter = setTimeout(loopDeleting, 100);
+    };
+    loopDeleting();
+};
+typeNow();
